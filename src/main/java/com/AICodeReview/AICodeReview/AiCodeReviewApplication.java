2c384958490811f3f8b8c42d6dc75e96d4a90a0f
Sharm <sharm@example.com>
2026-06-14 00:59:56 +0530

    Initial Commit

---
+++ src/main/java/com/AICodeReview/AICodeReview/AiCodeReviewApplication.java
@@ -0,0 +1,13 @@
+package com.AICodeReview.AICodeReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodeReviewApplication.class, args);
+	}
+
+}
